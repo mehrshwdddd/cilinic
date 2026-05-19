9fa951582880317051938f2afdad8a6446d048db
mehrshwdddd <mehrshad.asadi.a@gmail.com>
2026-05-19 10:26:41 +0430

    fixed Patient.php
    Signed-off-by: mehrshwdddd <mehrshad.asadi.a@gmail.com>

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Patient extends Model
 {
-    //
+    protected $fillable =[
+        'first_name',
+        'last_name',
+        'phone_number',
+        'national_code',
+        'phone_number',
+    ];
 }
