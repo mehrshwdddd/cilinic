f445b84231d0b447a16d3ca2a3bedc6f5f307bf7
mehrshwdddd <mehrshad.asadi.a@gmail.com>
2026-05-20 13:40:12 +0430

    show map in footer section
    Signed-off-by: mehrshwdddd <mehrshad.asadi.a@gmail.com>

--- resources/views/front/partials/footer.blade.php
+++ resources/views/front/partials/footer.blade.php
@@ -1,21 +1,68 @@
-<div class="bg-gray-900 text-gray-300 py-8 mt-10">
+<div class="bg-gray-900 text-gray-300 py-10 mt-10">
+
     <div class="max-w-6xl mx-auto px-6 grid md:grid-cols-3 gap-8">
+
         <div>
-            <h3 class="text-white font-semibold mb-3">address</h3>
-            <p>{{$setting->address ?? ''}}</p>
+            <h3 class="text-white font-semibold mb-3 text-lg">
+                Address
+            </h3>
+
+            <p class="leading-7">
+                {{ $setting->address ?? '' }}
+            </p>
         </div>
 
         <div>
-            <h3 class="text-white font-semibold mb-3">phone number</h3>
-            <p>{{$setting->phone ?? ''}}</p>
-        </div>
-        <div>
-            <h3 class="text-white font-semibold mb-3">clinic name</h3>
-            <p>{{$setting->clinic_name ?? ''}}</p>
+            <h3 class="text-white font-semibold mb-3 text-lg">
+                Phone Number
+            </h3>
+
+            <p>
+                {{ $setting->phone ?? '' }}
+            </p>
         </div>
 
-        <div class="max-w-6xl mx-auto px-6 mt-8">
-             {{$setting->map_address ?? ''}}
+        <div>
+            <h3 class="text-white font-semibold mb-3 text-lg">
+                Clinic Name
+            </h3>
+
+            <p>
+                {{ $setting->clinic_name ?? '' }}
+            </p>
         </div>
+
     </div>
+
+    <div class="max-w-6xl mx-auto px-6 mt-10">
+
+        <div id="footerMap"
+             class="w-full h-[400px] rounded-3xl overflow-hidden border border-gray-700 shadow-xl">
+        </div>
+
+    </div>
+
 </div>
+
+<link rel="stylesheet"
+      href="https://static.neshan.org/sdk/leaflet/1.4.0/leaflet.css"/>
+
+<script src="https://static.neshan.org/sdk/leaflet/1.4.0/leaflet.js"></script>
+
+<script>
+
+    const lat = {{ $setting->map_lat ?? 35.699739 }};
+    const lng = {{ $setting->map_lng ?? 51.338097 }};
+
+    const footerMap = new L.Map('footerMap', {
+        key: 'web.6046451f609047d98e1722e58f4e5baf',
+        maptype: 'dreamy',
+        poi: true,
+        traffic: false,
+        center: [lat, lng],
+        zoom: 15
+    });
+
+    L.marker([lat, lng]).addTo(footerMap);
+
+</script>
